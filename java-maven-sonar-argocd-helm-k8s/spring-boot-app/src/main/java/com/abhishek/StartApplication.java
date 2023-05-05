487e9f0304113ac438aa669a5430eb70f43b87c5
Vijin Vinod <vfcvijin@gmail.com>
2023-05-05 16:08:08 +0530

    nthing

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "Hello World 1");
+        model.addAttribute("msg", "Hello World 11");
         return "index";
     }
 
